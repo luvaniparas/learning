359046c6b8dfe5831b7f9947148ed33af5a1b83b
Paras Luvani <luvaniparas5@gmail.com>
2023-02-13 13:17:03 +0530

    [React] :: Floating Components

--- React/src/Components/Intro/Intro.jsx
+++ React/src/Components/Intro/Intro.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import FloatingDiv from '../FloatingDiv/FloatingDev.jsx';
 import './Intro.css';
 import Github from '../../img/github.png';
 import LinkedIn from '../../img/linkedin.png';
@@ -7,9 +8,9 @@ import Instagram from '../../img/instagram.png';
 import Vector1 from '../../img/Vector1.png';
 import Vector2 from '../../img/Vector2.png';
 import Boy from '../../img/boy.png';
-// import Thumbup from '../../img/thumbup.png';
-// import Crown from '../../img/crown.png';
-// import Glassesimoji from '../../img/glassesimoji.png';
+import Crown from '../../img/crown.png';
+import Glassesimoji from '../../img/glassesimoji.png';
+import Thumbup from '../../img/thumbup.png';
 
 const Intro = () =>{
     return(
@@ -43,9 +44,19 @@ const Intro = () =>{
                 <img className="i-Vector1" src={Vector1} alt="Vector1" />
                 <img className="i-Vector2" src={Vector2} alt="Vector2" />
                 <img className="i-Boy" src={Boy} alt="Boy" />
-                {/* <img src={Thumbup} alt="Thumbup" />
-                <img src={Crown} alt="Crown" />
-                <img src={Glassesimoji} alt="Glassesimoji" /> */}
+                <img className="i-Glassesimoji" src={Glassesimoji} alt="Glassesimoji" />
+
+                <div style={{top:'-4%',left:'68%'}}>
+                    <FloatingDiv image={Crown} txt1="Web" txt2="Developer"/>
+                </div>
+
+                <div style={{top:'18rem',left: '0rem'}}>
+                    <FloatingDiv image={Thumbup} txt1="Best Design" txt2="Award"/>
+                </div>
+
+                <div className="blur" style={{background: "rgb(238 210 255)"}}></div>
+
+                <div className="blur" style={{background: "#C1F5FF",top: '17rem', width: '21rem',height:'11rem',left:"-9rem"}}></div>
             </div>
         </div>
     )
